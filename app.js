2eebc173afa9f257d237379fec57844fc31252ed
Karthik raja <karthikrajdev1999@gmail.com>
2020-06-28 02:34:56 +0530

    whitelist

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use('/api', productRoutes);
 app.use('/api', stripeRoutes);
 app.use('/api', orderRoutes);
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT ;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
